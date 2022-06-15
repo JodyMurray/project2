8dde2f5c52be60d5a4f0e42b38976f734913175e
Jody M <jodyamm@hotmail.com>
2022-06-15 05:49:03 +0000

    Add button and styling

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var crystalBall = document.getElementsByClassName('crystal-ball');
+var answerSection = document.getElementById('answerSection');
+var answer = document.getElementById('answers');
+
+var input = document.getElementById('input');
+var ball = document.getElementById('ball');
