317e883b0496089b446cbd73d0d5a4a8cf13908e
Jody M <jodyamm@hotmail.com>
2022-06-15 11:06:30 +0000

    Add js to try get ball to shake

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
-var crystalBall = document.getElementsByClassName('crystal-ball');
-var answerSection = document.getElementById('answerSection');
-var answer = document.getElementById('answers');
+function shake(){
+var messageText = document.getElementById("message"),{
 
-var input = document.getElementById('input');
-var ball = document.getElementById('ball');
+document.getElementById('ball') = shake();
+
+setTimeout(function(){ball.classList.remove("shake");}, 1500);
+
+setTimeout(function(){getFortune();}, 1500);
+}
+}
+function getFortune() {
+    var fortunes = ['Absolutely so', 'Certainly not', 'Signs point to yes', 'Unable to read now', 'Ask again later', 'Of course!'
+    , 'Cannot predict now', 'You may rely on it', 'Outlook does not look good', 'Very doubtful', 'Most likely', 'No',
+    'Reply hazy, try again later', 'My sources say no', 'Yes, as I see it']
+
+    var fortune = fortunes[Math.floor(Math.random()*fortunes.length)];
+
+
+var parent = document.getElementById("fortune");
+var newMessage = document.createElement("div");
+newMessage.setAttribute('id', "message");
+newMessage.innerHTML = "\""+fortune+"\"";
+parent.appendChild(newMessage);
+}
+    
